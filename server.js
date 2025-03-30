163fe9a0a95168f3f5b68d5a3434979fe3557df5
alexvtl <alexandre.vital@hotmail.fr>
2025-03-30 13:31:13 +0200

    test 3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Middleware pour parser le JSON
 app.use(express.json());
