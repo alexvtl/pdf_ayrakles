22683b76ee9627a00c82b87da3abd4ff3287c324
alexvtl <alexandre.vital@hotmail.fr>
2025-03-31 12:03:49 +0200

    test 26

--- render-build.sh
+++ render-build.sh
@@ -9,5 +9,5 @@ npx puppeteer browsers install chrome
 
 # Récupère le chemin exact de Chrome et le sauvegarde dans un fichier .env
 CHROME_PATH=$(npx puppeteer executable-path chrome)
-echo "CHROME_PATH=$CHROME_PATH" > .chrome-path.env
+echo "CHROME_PATH=$CHROME_PATH"
 echo "✅ Chrome installé ici : $CHROME_PATH"
