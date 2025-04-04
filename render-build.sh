633bdb44561b41fd50c2ad9af52c2026120f3a9b
alexvtl <alexandre.vital@hotmail.fr>
2025-04-04 16:16:42 +0200

    i sass

--- render-build.sh
+++ render-build.sh
@@ -9,6 +9,11 @@ npm install
 echo "📁 Création du dossier .cache (si non existant)..."
 mkdir -p .cache/puppeteer
 
+npm install -g sass
+
+# 📦 Installer les dépendances de développement
+sass .front_template_devis/style.scss style.css
+
 # 🧭 Définir la variable d'environnement de cache
 export PUPPETEER_CACHE_DIR="$(pwd)/.cache/puppeteer"
 echo "📍 PUPPETEER_CACHE_DIR=$PUPPETEER_CACHE_DIR"
