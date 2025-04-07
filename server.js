969b0886ed9c7ad693e38517b1c7c95a1ac954b8
alexvtl <alexandre.vital@hotmail.fr>
2025-04-07 12:03:12 +0200

    bug reponse

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 const { renderTableaux } = require("./tableau.js");
-const { getImageBase64 } = require("./imagesBase64.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
@@ -28,7 +27,7 @@ app.post("/generate-pdf", async (req, res) => {
   // créer tableaux
   const table = renderTableaux(data);
   // créer image logo
-  const imageslogo = `<img style="object-fit: cover;height: 4cm;width=100%;" src="data:image/png;base64,${data.logo}" />`;
+  const imageslogo = `<img style="object-fit: cover;height: 4cm;width:100%;" src="data:image/png;base64,${data.logo}" />`;
   // 📁 Lire le HTML brut
   const htmlPath = path.join(__dirname, "./front_template_devis/index.html");
   let html = fs.readFileSync(htmlPath, "utf-8");
@@ -41,7 +40,7 @@ app.post("/generate-pdf", async (req, res) => {
   const css = fs.readFileSync(cssPath, "utf-8");
 
   // 🖼️ Remplacer {{table}} par le tableau HTML
-  htmlPage = html
+  const htmlPage = html
     .replace("</head>", `<style>${css}</style></head>`)
     .replace("{{client_name}}", data.client_name)
     .replace("{{client_adresse_1}}", data.client_adresse_1)
