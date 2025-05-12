f988d738b9d4f6a67563f755ef8327986b445300
alexvtl <alexandre.vital@hotmail.fr>
2025-05-12 12:26:54 +0200

    ajout avenant

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 const { renderTableaux } = require("./tableau_devis.js");
 const { tableau_total, tableau_facture } = require("./tableau_facture.js");
+const { renderTableaux_avenant } = require("./tableau_avenant.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require("fs");
@@ -156,7 +157,7 @@ app.post("/generate-pdf/facture", async (req, res) => {
 
       footerTemplate: `
           <div style=" position:absolute; bottom: 0.5cm; left:0cm; margin:0 0.5cm;display: flex;font-size:13px;flex-direction: column;justify-content: center; width: 20cm;">
-<span style="-webkit-print-color-adjust: exact; color:black;align-self: center; margin: 0;text-align: center;width: 90%;margin: auto;">
+          <span style="-webkit-print-color-adjust: exact; color:black;align-self: center; margin: 0;text-align: center;width: 90%;margin: auto;">
           ${data.entreprise_nom} - ${data.entreprise_adresse_1} -
           ${data.entreprise_adresse_2} - Capital de ${data.entreprise_capital_social} €
           SIREN ${data.entreprise_siren} - contrat d’assurance :
@@ -166,6 +167,77 @@ app.post("/generate-pdf/facture", async (req, res) => {
       </div>
         `,
       margin: { top: "0.5cm", bottom: "0.5cm", left: "0.5cm", right: "0.5cm" },
+    });
+
+    res.set({
+      "Content-Type": "application/pdf",
+    });
+    console.log(` Pdf genéré par : ${data.user_id} pdf_id : ${uuid}`);
+
+    res.end(pdfbuffer);
+
+    await browser.close();
+  } catch (err) {
+    console.error("Erreur PDF :", err);
+    res.status(500).send("Erreur lors de la génération du PDF");
+  }
+});
+
+app.post("/generate-pdf/avenant", async (req, res) => {
+  const data = req.body || [];
+  // créer tableaux
+  const table = renderTableaux_avenant(data);
+  // créer image logo
+  const imageslogo = `<img style="object-fit: cover;height: 4cm;max-width:9cm;" src="data:image/${data.logo_type};base64,${data.logo}" />`;
+  // 📁 Lire le HTML brut
+  const htmlPath = path.join(__dirname, "./front_template_avenant/index.html");
+  let html = fs.readFileSync(htmlPath, "utf-8");
+  // 🆔 Générer un UUID pour le nom du fichier
+  const uuid = uuidv4();
+
+  // const filePath = path.join(__dirname, "files", `${uuid}.pdf`);
+  // 🎨 Lire et injecter le CSS dans un <style>
+  const cssPath = path.join(__dirname, "./front_template_devis/style.css");
+  const css = fs.readFileSync(cssPath, "utf-8");
+
+  // 🖼️ Remplacer {{table}} par le tableau HTML
+  const htmlPage = html
+    .replace("</head>", `<style>${css}</style></head>`)
+    .replace("{{client_name}}", data.client_name)
+    .replace("{{client_adresse_1}}", data.client_adresse_1)
+    .replace("{{client_adresse_2}}", data.client_adresse_2)
+    .replace("{{date}}", data.date)
+    .replace("{{type_projet}}", data.type_projet)
+    .replace("{{bien_lieu}}", data.bien_lieu)
+    .replace("{{table}}", table)
+    .replace("{{imagelogo}}", imageslogo);
+
+  try {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    await page.setContent(htmlPage, { waitUntil: "networkidle0" });
+    const pdfbuffer = await page.pdf({
+      format: "A4",
+      printBackground: true,
+      displayHeaderFooter: true,
+      headerTemplate: `<div style=" position:absolute; top:1cm; left:1cm; width:19cm; margin:0cm  0cm 0cm 0cm; display:flex; justify-content:space-between;">
+        <span style="-webkit-print-color-adjust: exact; font-size: 22px; color: #eccc8c;font-weight: bold;">AVENANT</span>
+        <span style=" font-size: 12px; color: #296b77;font-weight: bold;">Avenant n°${data.numero_devis}</span>
+       </div>`,
+
+      footerTemplate: `
+          <div style=" position:absolute; bottom:2.2cm;  width:15cm; margin:0; left: 50%;transform: translateX(-50%);
+        right: 50%;  display:flex; justify-content:space-between; align-items:center;">
+          <span style="-webkit-print-color-adjust: exact; font-size:13px;color:rgb(168, 168, 168); max-width:65%">${data.nom_entreprise}, ${data.adresse_entreprise} – ${data.forme_juridique} – CAPITAL SOCIAL ${data.capital_social} –
+            SIREN ${data.siren} – DECENNALE ${data.assurance_nom} N° CONTRAT : ${data.contrat_decennale} –
+            EMAIL : ${data.email_entreprise} / TEL : ${data.telephone_entreprise}
+          </span>
+          <span style="-webkit-print-color-adjust: exact; font-size:16px ;font-weight:bold ;color: #296b77" class="pageNumber"></span>
+          </div>
+        `,
+      margin: { top: "60px", bottom: "4cm" },
     });
 
     res.set({
---
+++ tableau_avenant.js
@@ -0,0 +1,162 @@
+const renderTableaux_avenant = (Data) => {
+  let tableaux = `
+ <table class="tableau_prestations">
+ <colgroup>
+    <col style="width: 9cm">
+    <col style="width: 2cm">
+    <col style="width: 1cm">
+    <col style="width: 2.5cm">
+    <col style="width: 0.5cm">
+    <col style="width: 3cm">
+  </colgroup>
+    <tr class="tableau_prestations__header">
+      <th class="tableau_prestations__header__libelle">Libellé</th>
+      <th>Quantité</th>
+      <th>Unité</th>
+      <th>PU HT</th>
+      <th>TVA</th>
+      <th class="bold tableau_prestations__header__total">Total HT</th>
+    </tr>`;
+
+  Data.lots.forEach((lot) => {
+    let html = `
+     <tr class="section__lot">
+      <td colspan="5" class="section__lot__name td_padding">${lot.name}</td>
+      <td class="section__lot__total td_padding total">${lot.lots_sum}</td>
+    </tr>
+      
+    `;
+
+    lot.pieces.forEach((piece) => {
+      html += `
+    <tr class="section__piece">
+      <td colspan="5" class="section__piece__name">${piece.name}</td>
+      <td class="section__piece__price">${piece.pieces_sum}</td>
+    </tr>
+      `;
+
+      piece.prestations.forEach((presta) => {
+        html += `
+          <tr class="section__prestation">
+            <td colspan="1" class="section__prestation__info td_padding">
+              <div class="section__prestation__info__name">${presta.name}</div>
+              <div class="section__prestation__info__description">${
+                presta.description || ""
+              }</div>
+            </td>
+            <td class="section__prestation__quantity td_padding">${
+              presta.quantity
+            }</td>
+            <td class="section__prestation__unit td_padding">${
+              presta.unite
+            }</td>
+            <td class="section__prestation__price_uht td_padding">${
+              presta.puht
+            }</td>
+            <td class="section__prestation__remise td_padding">${
+              presta.tva
+            }</td>
+            <td class="section__prestation__price td_padding">${
+              presta.ptht
+            }</td>
+          </tr>
+        `;
+      });
+    });
+    tableaux += html;
+  });
+
+  if (Data.lots_option.length > 0) {
+    Data.lots_option.forEach((lot) => {
+      let html_option = `
+     <tr class="section__lot">
+      <td colspan="5" class="section__lot__name td_padding">${lot.name} (Option)</td>
+      <td class="section__lot__total td_padding total">${lot.lots_sum}</td>
+    </tr> 
+    `;
+
+      lot.pieces.forEach((piece) => {
+        html_option += `
+    <tr class="section__piece">
+      <td colspan="5" class="section__piece__name">${piece.name} (Option)</td>
+      <td class="section__piece__price">${piece.pieces_sum}</td>
+    </tr>
+      `;
+
+        piece.prestations.forEach((presta) => {
+          html_option += `
+          <tr class="section__prestation">
+            <td colspan="1" class="section__prestation__info td_padding">
+              <div class="section__prestation__info__name">${presta.name}</div>
+              <div class="section__prestation__info__description">${
+                presta.description || ""
+              }</div>
+            </td>
+            <td class="section__prestation__quantity td_padding">${
+              presta.quantity
+            }</td>
+            <td class="section__prestation__unit td_padding">${
+              presta.unite
+            }</td>
+            <td class="section__prestation__price_uht td_padding">${
+              presta.puht
+            }</td>
+            <td class="section__prestation__remise td_padding">${
+              presta.tva
+            }</td>
+            <td class="section__prestation__price td_padding">${
+              presta.ptht
+            }</td>
+          </tr>
+        `;
+        });
+      });
+      tableaux += html_option;
+    });
+    tableaux += `</table>`;
+  } else {
+    tableaux += `</table>`;
+  }
+
+  let tableaux_totaux = `
+ 
+  <table class="table_totaux__hors_option">
+   <colgroup>
+    <col style="width: 8cm">
+    <col style="width: 3cm">
+  </colgroup>
+   <thead>
+    <tr>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr class="table_totaux__hors_option__total_ht">
+      <td colspan="1" class="table_totaux__hors_option__total_ht__text td_padding green first_column">Total HT (hors options)</td>
+      <td class="table_totaux__hors_option__total_ht__montant td_padding second_column">${Data.totaux.hors_option.montant_ht} €</td>
+  </tr> 
+  `;
+  Data.totaux.hors_option.tva.forEach((tva) => {
+    tableaux_totaux += `
+    <tr class="table_totaux__hors_option__tva">
+      <td colspan="1" class="table_totaux__hors_option__tva__text td_padding green first_column">TVA ${tva.taux} %</td>
+      <td class="table_totaux__hors_option__tva__montant td_padding second_column">${tva.montant} €</td>
+    </tr>
+    `;
+  });
+  tableaux_totaux += `
+  <tr class="table_totaux__hors_option__total_ttc">
+    <td colspan="1" class="table_totaux__hors_option__total_ttc__text green td_padding first_column">Total ttc (hors options)</td>
+    <td class="table_totaux__hors_option__total_ttc__montant td_padding second_column yellow">${Data.totaux.hors_option.montant_ttc} €</td>
+  </tr>
+  </tbody>
+  </table>
+  `;
+
+  tableaux += `<div class="table_totaux">${tableaux_totaux}</div>`;
+
+  return tableaux;
+};
+
+module.exports = { renderTableaux_avenant };
