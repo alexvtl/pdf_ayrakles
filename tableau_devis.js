a9bf09117bfab7ddb0cff468362cfd0b97f73bba
alexvtl <alexandre.vital@hotmail.fr>
2025-09-11 19:25:47 +0200

    test design 10

--- tableau_devis.js
+++ tableau_devis.js
@@ -119,139 +119,139 @@ const renderTableaux = (Data) => {
     tableaux += `</table>`;
   }
 
-  let tableaux_totaux = `
- 
-  <table class="table_totaux__hors_option">
-   <colgroup>
-    <col style="width: 8cm">
-    <col style="width: 3cm">
-  </colgroup>
-   <thead>
-    <tr>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-  <tr class="table_totaux__hors_option__total_ht">
-      <td colspan="1" class="table_totaux__hors_option__total_ht__text td_padding green first_column">Total HT (hors options)</td>
-      <td class="table_totaux__hors_option__total_ht__montant td_padding second_column">${Data.totaux.hors_option.montant_ht} €</td>
-  </tr> 
-  `;
-  Data.totaux.hors_option.tva.forEach((tva) => {
-    tableaux_totaux += `
-    <tr class="table_totaux__hors_option__tva">
-      <td colspan="1" class="table_totaux__hors_option__tva__text td_padding green first_column">TVA ${tva.taux} %</td>
-      <td class="table_totaux__hors_option__tva__montant td_padding second_column">${tva.montant} €</td>
-    </tr>
-    `;
-  });
-  tableaux_totaux += `
-  <tr class="table_totaux__hors_option__total_ttc">
-    <td colspan="1" class="table_totaux__hors_option__total_ttc__text green td_padding first_column">Total ttc (hors options)</td>
-    <td class="table_totaux__hors_option__total_ttc__montant td_padding second_column yellow">${Data.totaux.hors_option.montant_ttc} €</td>
-  </tr>
-  </tbody>
-  </table>
-  `;
-  if (Data.totaux.option.montant_ht) {
-    tableaux_totaux += `
-  <table class="table_totaux__option">
-  <colgroup>
-    <col style="width: 8cm">
-    <col style="width: 3cm">
-  </colgroup>
-  <thead>
-    <tr>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="table_totaux__option__total_ht">
-      <td colspan="1" class="table_totaux__option__total_ht__text green td_padding first_column">Total HT (options)</td>
-      <td class="table_totaux__option__total_ht__montant td_padding second_column">${Data.totaux.option.montant_ht} €</td>
-    </tr>
-    `;
-    Data.totaux.option.tva.forEach((tva) => {
-      tableaux_totaux += `
-      <tr class="table_totaux__option__tva">
-        <td colspan="1" class="table_totaux__option__tva__text green td_padding first_column">TVA ${tva.taux} %</td>
-        <td class="table_totaux__option__tva__montant second_column">${tva.montant}</td>
-      </tr>
-      `;
-    });
-    tableaux_totaux += `
-    <tr class="table_totaux__option__total_ttc">
-      <td colspan="1" class="table_totaux__option__total_ttc__text green td_padding first_column">Total ttc (options)</td>
-      <td class="table_totaux__option__total_ttc__montant second_column yellow">${Data.totaux.option.montant_ttc}</td>
-    </tr>
-    </tbody>
-    </table>`;
-  } else {
-    null;
-  }
-  tableaux_totaux += `
-  <table class="table_date">
-  <colgroup>
-    <col style="width: 8cm">
-    <col style="width: 3cm">
-  </colgroup>
-  <thead>
-    <tr>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="table_date__date_demarrage">
-      <td colspan="1" class="table_date__date_demarrage__text green td_padding first_column">Date de démarrage envisagée</td>
-      <td class="table_date__date_demarrage__date td_padding second_column">${Data.date_demarrage}</td>
-    </tr>
-    <tr class="table_date__date_duree">
-      <td colspan="1" class="table_date__date_duree__text green td_padding first_column">Durée des travaux estimée</td>
-      <td class="table_date__date_duree__montant td_padding second_column">${Data.date_duree}</td>
-    </tr>
-  </tbody>
-  </table>
-  `;
-  tableaux_totaux += `
-  <section class="section__acompte">
-  <div class="section__acompte__title">
-  Conditions de paiement
-  </div>
-  <table class="table_acompte">
-  <colgroup>
-    <col style="width: 15cm">
-    <col style="width: 3cm">
-    <col style="width: 3cm">
-      
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-  `;
-  Data.acompte.forEach((acompte) => {
-    tableaux_totaux += `
-        <tr class="table_acompte__acompte_tr">
-          <td colspan="1" class="table_acompte__acompte_tr__text ">${acompte.nom}</td>
-          <td class="table_acompte__acompte_tr__tva ">${acompte.taux} %</td>  
-          <td class="table_acompte__acompte_tr__montant second_column_acompte">${acompte.montant} ttc</td>
-        </tr>
-      `;
-  });
-  tableaux_totaux += `
-  </tbody>
-  </table>
-   
-  </section>
-  `;
+  // let tableaux_totaux = `
 
-  tableaux += `<div class="table_totaux">${tableaux_totaux}</div>`;
+  // <table class="table_totaux__hors_option">
+  //  <colgroup>
+  //   <col style="width: 8cm">
+  //   <col style="width: 3cm">
+  // </colgroup>
+  //  <thead>
+  //   <tr>
+  //     <th></th>
+  //     <th></th>
+  //   </tr>
+  // </thead>
+  // <tbody>
+  // <tr class="table_totaux__hors_option__total_ht">
+  //     <td colspan="1" class="table_totaux__hors_option__total_ht__text td_padding green first_column">Total HT (hors options)</td>
+  //     <td class="table_totaux__hors_option__total_ht__montant td_padding second_column">${Data.totaux.hors_option.montant_ht} €</td>
+  // </tr>
+  // `;
+  // Data.totaux.hors_option.tva.forEach((tva) => {
+  //   tableaux_totaux += `
+  //   <tr class="table_totaux__hors_option__tva">
+  //     <td colspan="1" class="table_totaux__hors_option__tva__text td_padding green first_column">TVA ${tva.taux} %</td>
+  //     <td class="table_totaux__hors_option__tva__montant td_padding second_column">${tva.montant} €</td>
+  //   </tr>
+  //   `;
+  // });
+  // tableaux_totaux += `
+  // <tr class="table_totaux__hors_option__total_ttc">
+  //   <td colspan="1" class="table_totaux__hors_option__total_ttc__text green td_padding first_column">Total ttc (hors options)</td>
+  //   <td class="table_totaux__hors_option__total_ttc__montant td_padding second_column yellow">${Data.totaux.hors_option.montant_ttc} €</td>
+  // </tr>
+  // </tbody>
+  // </table>
+  // `;
+  // if (Data.totaux.option.montant_ht) {
+  //   tableaux_totaux += `
+  // <table class="table_totaux__option">
+  // <colgroup>
+  //   <col style="width: 8cm">
+  //   <col style="width: 3cm">
+  // </colgroup>
+  // <thead>
+  //   <tr>
+  //     <th></th>
+  //     <th></th>
+  //   </tr>
+  // </thead>
+  // <tbody>
+  //   <tr class="table_totaux__option__total_ht">
+  //     <td colspan="1" class="table_totaux__option__total_ht__text green td_padding first_column">Total HT (options)</td>
+  //     <td class="table_totaux__option__total_ht__montant td_padding second_column">${Data.totaux.option.montant_ht} €</td>
+  //   </tr>
+  //   `;
+  //   Data.totaux.option.tva.forEach((tva) => {
+  //     tableaux_totaux += `
+  //     <tr class="table_totaux__option__tva">
+  //       <td colspan="1" class="table_totaux__option__tva__text green td_padding first_column">TVA ${tva.taux} %</td>
+  //       <td class="table_totaux__option__tva__montant second_column">${tva.montant}</td>
+  //     </tr>
+  //     `;
+  //   });
+  //   tableaux_totaux += `
+  //   <tr class="table_totaux__option__total_ttc">
+  //     <td colspan="1" class="table_totaux__option__total_ttc__text green td_padding first_column">Total ttc (options)</td>
+  //     <td class="table_totaux__option__total_ttc__montant second_column yellow">${Data.totaux.option.montant_ttc}</td>
+  //   </tr>
+  //   </tbody>
+  //   </table>`;
+  // } else {
+  //   null;
+  // }
+  // tableaux_totaux += `
+  // <table class="table_date">
+  // <colgroup>
+  //   <col style="width: 8cm">
+  //   <col style="width: 3cm">
+  // </colgroup>
+  // <thead>
+  //   <tr>
+  //     <th></th>
+  //     <th></th>
+  //   </tr>
+  // </thead>
+  // <tbody>
+  //   <tr class="table_date__date_demarrage">
+  //     <td colspan="1" class="table_date__date_demarrage__text green td_padding first_column">Date de démarrage envisagée</td>
+  //     <td class="table_date__date_demarrage__date td_padding second_column">${Data.date_demarrage}</td>
+  //   </tr>
+  //   <tr class="table_date__date_duree">
+  //     <td colspan="1" class="table_date__date_duree__text green td_padding first_column">Durée des travaux estimée</td>
+  //     <td class="table_date__date_duree__montant td_padding second_column">${Data.date_duree}</td>
+  //   </tr>
+  // </tbody>
+  // </table>
+  // `;
+  // tableaux_totaux += `
+  // <section class="section__acompte">
+  // <div class="section__acompte__title">
+  // Conditions de paiement
+  // </div>
+  // <table class="table_acompte">
+  // <colgroup>
+  //   <col style="width: 15cm">
+  //   <col style="width: 3cm">
+  //   <col style="width: 3cm">
+
+  //     <thead>
+  //       <tr>
+  //         <th></th>
+  //         <th></th>
+  //         <th></th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  // `;
+  // Data.acompte.forEach((acompte) => {
+  //   tableaux_totaux += `
+  //       <tr class="table_acompte__acompte_tr">
+  //         <td colspan="1" class="table_acompte__acompte_tr__text ">${acompte.nom}</td>
+  //         <td class="table_acompte__acompte_tr__tva ">${acompte.taux} %</td>
+  //         <td class="table_acompte__acompte_tr__montant second_column_acompte">${acompte.montant} ttc</td>
+  //       </tr>
+  //     `;
+  // });
+  // tableaux_totaux += `
+  // </tbody>
+  // </table>
+
+  // </section>
+  // `;
+
+  // tableaux += `<div class="table_totaux">${tableaux_totaux}</div>`;
 
   // Bloc récapitulatif style "Card + Grid" (équivalent Tailwind)
   let recap_card = `
